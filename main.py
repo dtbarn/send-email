304c89128f1b859fc563afb5bbde84cd62d17ef5
Dan <dbarnett@hotmail.com>
2021-11-16 20:20:18 +0000

    Initial

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import yagmail
-import os
+import json
 
-sender = 'dantbarnett@gmail.com'
-sender_pw = os.getenv('APP_PW')
+with open('config.json', 'r') as f:
+    config = json.load(f)
+ 
+sender = config['sender']
+sender_pw = config['password']
 receiver = 'dtbarnett@suddenlink.net'
 subject = 'Test subject'
-content = """This is the content for the test email application"""
+content = ["""This is the content for the test email application""", 'text.txt']
 
 yag = yagmail.SMTP(user=sender, password=sender_pw)
 yag.send(to=receiver, subject=subject, contents=content)
